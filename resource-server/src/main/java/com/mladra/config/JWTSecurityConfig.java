16b0940d402e4e8c9a1ae277148cdf94b7ced4fa
Michał Ladra <michal.ladra.95@gmail.com>
2021-01-12 10:38:10 +0100

    Enabled CORS requests for all endpoints

--- resource-server/src/main/java/com/mladra/config/JWTSecurityConfig.java
+++ resource-server/src/main/java/com/mladra/config/JWTSecurityConfig.java
@@ -1,9 +1,13 @@
 package com.mladra.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +16,12 @@ public class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests(authz -> authz.anyRequest().permitAll());
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 }
